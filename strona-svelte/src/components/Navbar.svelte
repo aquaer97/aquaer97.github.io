6116b3c63ab9972a3c9a3701ec494971d1b85a4f
aquaer97 <jakubgepfert2007@gmail.com>
2025-07-13 16:51:07 +0200

    work in progress

---
+++ strona-svelte/src/components/Navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<nav class="bg-black shadow-blue-950">
+  <div class=" mx-auto px-4 py-4">
+    <div class="flex justify-center items-center ">
+      <!-- Logo -->
+      <a href="/" class="text-white">
+        <h1 class="text-3xl font-bold ">aquaer97</h1>
+      </a>
+      <!-- Navigation Links -->
+       </div>
+      <!-- Navigation Links -->
+      <div class="inline-block  space-x-10 text-white mt-5 text-xl  ">
+        <a href="src/routes/games/+page.svelte">link</a> <a href="#">link</a> <a href="#">link</a>
+      </div>
+    </div>
+</nav>
