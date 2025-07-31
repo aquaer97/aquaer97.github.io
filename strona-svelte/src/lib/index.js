1a7aacebeeacb599761b26971b7ade2d475b6908
aquaer97 <jakubgepfert2007@gmail.com>
2025-07-31 19:12:44 +0200

    svelte deletion

--- strona-svelte/src/lib/index.js
+++
@@ -1,6 +0,0 @@
-import Footer from "./components/Footer.svelte";
-import Navbar from "./components/Navbar.svelte";
-
-// place files you want to import through the `$lib` alias in this folder.
-Navbar
-Footer
