610b5f0b3cff9a08a4a68d5c9fa024c4a95eea1e
aquaer97 <jakubgepfert2007@gmail.com>
2026-09-18 21:46:11 +0200

    indents etc.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # aquaer97.github.io
+
 Nieprofesjonalna, osobista i nieco porzucona strona od której widoku oczy płaczą, a na telefonie póki co nie działa, i pewnie nie będzie.
 
 # TO DO (18/05/2026)
+
 - movies.html
 - naprawienie strony, aby działała na telefonach
 - ...
--- felizjueves.js
+++ felizjueves.js
@@ -1,9 +1,9 @@
 const today = new Date();
-    const dayOfWeek = today.getDay();
+const dayOfWeek = today.getDay();
 
-    if (dayOfWeek === 4) {
-        document.getElementById('thursdayGif').style.display = 'block';
-    }
-    if (dayOfWeek === 5) {
-        document.getElementById('fridayMovie').style.display = 'block';
-    }
+if (dayOfWeek === 4) {
+	document.getElementById("thursdayGif").style.display = "block";
+}
+if (dayOfWeek === 5) {
+	document.getElementById("fridayMovie").style.display = "block";
+}
